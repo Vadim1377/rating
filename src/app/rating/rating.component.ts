6eeed2c6f3ef9d330ba289b343be749da3ad9be6
asda <garrapovv@mail.ru>
2022-05-06 11:47:29 +0300

    тщетные попытки добавить юбилей и кол-во дней до др

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -2,17 +2,17 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { HttpService} from '../http.service';
 import {TableModule} from 'primeng/table';
 import {User} from '../rating/user';
-
 @Component({
   selector: 'rating',
   templateUrl: './rating.component.html',
-  styleUrls: ['./rating.component.css']
+  styleUrls: ['./rating.component.css'],
 })
 
 export class RatingComponent {
  @Output() selectName = new EventEmitter<number>();
   title = 'rating';
   list!: User[];
+  currentDate = new Date();
   detailuserid: any;
   constructor(private httpService: HttpService){}
       
@@ -51,6 +51,20 @@ export class RatingComponent {
     openDetailUser(val: number){
       this.selectName.emit(val);
       console.log(val)
-    }
+  }
+   inBetween(date1:Date, date2:Date):number {
+    //Get 1 day in milliseconds
+    var one_day = 1000 * 60 * 60 * 24;
+
+    // Convert both dates to milliseconds
+    var date1_ms = date1.getTime();
+    var date2_ms = date2.getTime();
+
+    // Calculate the difference in milliseconds
+    var difference_ms = date2_ms - date1_ms;
+
+    // Convert back to days and return
+    return Math.round(difference_ms / one_day);
+  }
 }
 
